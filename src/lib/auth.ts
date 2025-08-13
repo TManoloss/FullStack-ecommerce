bd734e89347709da1f3cf632c15a2c5ef56b95ac
Manoel Carvalho <manoelfelipe90@gmail.com>
2025-08-13 14:52:02 -0300

    feat: add login with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,6 +7,12 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   database: drizzleAdapter(db, { provider: "pg", schema }),
   user: {
     modelName: "userTable",
@@ -16,5 +22,8 @@ export const auth = betterAuth({
   },
   account: {
     modelName: "accountTable",
+  },
+  verification: {
+    modelName: "verificationTable",
   },
 });
